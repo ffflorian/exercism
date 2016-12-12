a3ecd3b561a45b0fc4498ec14a683213ab74394e
Florian Keller <github@floriankeller.de>
2016-12-12 16:06:05 +0100

    Improved solution for #17

--- 17-space-age/space-age.js
+++ 17-space-age/space-age.js
@@ -1,46 +1,26 @@
 module.exports = function (seconds) {
     'use strict';
 
-    const divideSecondsBy = (divider) => {
-        return parseFloat((seconds / divider).toFixed(2));
+    const yearOnEarth = 31557600;
+
+    const planets = {
+        Earth: 1,
+        Jupiter: 11.862615,
+        Mars: 1.8808158,
+        Mercury: 0.2408467,
+        Neptune: 164.79132,
+        Saturn: 29.447498,
+        Uranus: 84.016846,
+        Venus: 0.61519726
     };
 
-    const spaceTimes = {
-        earth: 31557600,
-        jupiter: 374222565,
-        mars: 59359776,
-        mercury: 7600526,
-        neptune: 5196280668,
-        saturn: 928792570,
-        uranus: 2652994592,
-        venus: 19411026
-    };
-
-    return {
-        seconds: seconds,
-        onEarth () {
-            return divideSecondsBy(spaceTimes.earth);
-        },
-        onJupiter () {
-            return divideSecondsBy(spaceTimes.jupiter);
-        },
-        onMars () {
-            return divideSecondsBy(spaceTimes.mars);
-        },
-        onMercury () {
-            return divideSecondsBy(spaceTimes.mercury);
-        },
-        onNeptune () {
-            return divideSecondsBy(spaceTimes.neptune);
-        },
-        onSaturn () {
-            return divideSecondsBy(spaceTimes.saturn);
-        },
-        onUranus () {
-            return divideSecondsBy(spaceTimes.uranus);
-        },
-        onVenus () {
-            return divideSecondsBy(spaceTimes.venus);
-        }
-    };
+    return Object.assign({seconds}, (() => {
+        return Object.keys(planets).reduce((list, planet) =>
+            Object.assign(list, {
+                [`on${planet}`]: () => {
+                    return parseFloat((seconds / yearOnEarth / planets[planet]).toFixed(2));
+                }
+            }),
+        {});
+    })());
 };
