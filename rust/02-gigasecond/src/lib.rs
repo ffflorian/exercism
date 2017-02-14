fcef6003522d2878093dfa620e6f93d49cb99e44
Florian Keller <github@floriankeller.de>
2017-02-14 17:11:36 +0100

    Cleanup

--- rust/02-gigasecond/src/lib.rs
+++ rust/02-gigasecond/src/lib.rs
@@ -2,5 +2,5 @@ extern crate chrono;
 use chrono::*;
 
 pub fn after(start_date: DateTime<UTC>) -> DateTime<UTC> {
-    return start_date.to_owned() + Duration::seconds(1_000_000_000);
+    start_date + Duration::seconds(1_000_000_000)
 }
