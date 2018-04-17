304eae01641b130144d34fd43958d2bdbfe3252e
Florian Keller <github@floriankeller.de>
2018-04-17 14:06:29 +0200

    Add solution for JS #24

---
+++ javascript/24-roman-numerals/roman-numerals.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const map = [
+  1000, 'M',
+  900, 'CM',
+  500, 'D',
+  400, 'CD',
+  100, 'C',
+  90, 'XC',
+  50, 'L',
+  40, 'XL',
+  10, 'X',
+  9, 'IX',
+  5, 'V',
+  4, 'IV',
+  1, 'I'
+];
+
+module.exports = number => {
+  if (number < 1 || number > 3999) {
+    throw new Error('Invalid number');
+  }
+  let value = '';
+  for (let i = 0; number > 0 && i < map.length; i += 2) {
+    while (number >= map[i]) {
+      value += map[i + 1];
+      number -= map[i];
+    }
+  }
+  return value;
+};
