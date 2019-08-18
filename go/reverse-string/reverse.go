9f8bba9c96be494770e91c8671e975cadf159253
Florian Keller <github@floriankeller.de>
2019-08-18 12:53:16 +0200

    chore(go): Small format update

--- go/reverse-string/reverse.go
+++ go/reverse-string/reverse.go
@@ -1,8 +1,8 @@
 package reverse
 
 /*
-Reverse reverses a string
-*/
+ * Reverse reverses a string
+ */
 func Reverse(input string) string {
 	r := []rune(input)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
