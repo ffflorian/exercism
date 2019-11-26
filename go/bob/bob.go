eb189aaea3125121d8ba3e3c20c715c39df1d468
Florian Keller <github@floriankeller.de>
2019-11-26 21:33:23 +0100

    no need for new variable

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -4,20 +4,20 @@ import "strings"
 
 // Hey returns Bob's answer
 func Hey(remark string) string {
-	input := strings.TrimSpace(remark)
+	remark = strings.TrimSpace(remark)
 
-	if len(input) == 0 {
+	if len(remark) == 0 {
 		return "Fine. Be that way!"
 	}
 
-	if input == strings.ToUpper(input) && strings.ToUpper(input) != strings.ToLower(input) {
-		if strings.HasSuffix(input, "?") {
+	if remark == strings.ToUpper(remark) && strings.ToUpper(remark) != strings.ToLower(remark) {
+		if strings.HasSuffix(remark, "?") {
 			return "Calm down, I know what I'm doing!"
 		}
 		return "Whoa, chill out!"
 	}
 
-	if strings.HasSuffix(input, "?") {
+	if strings.HasSuffix(remark, "?") {
 		return "Sure."
 	}
 
