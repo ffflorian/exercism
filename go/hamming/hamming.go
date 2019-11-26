2b4d8da36ed36098df82cd87e7e2d0b5ef786972
Florian Keller <github@floriankeller.de>
2019-11-26 21:26:24 +0100

    simplify hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -4,19 +4,11 @@ import "errors"
 
 // Distance calculates the Hamming Distance between two DNA strands.
 func Distance(a, b string) (int, error) {
-	if a == "" && b != "" {
-		return 0, errors.New("left strand must not be empty")
-	}
-
-	if a != "" && b == "" {
-		return 0, errors.New("right strand must not be empty")
-	}
-
 	if len(a) != len(b) {
-		return 0, errors.New("left and right strands must be of equal length")
+		return -1, errors.New("left and right strands must be of equal length")
 	}
 
-	var diff = 0
+	diff := 0
 
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
