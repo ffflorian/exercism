670b12c63c6451ccc4c5f40a402c44cdd86a68da
Florian Keller <github@floriankeller.de>
2019-12-02 21:54:04 +0100

    chore: Fix test script

--- bin/test_all.sh
+++ bin/test_all.sh
@@ -8,15 +8,12 @@ RUN_COMMAND="${2} ${3}"
 for DIR in *; do
   if [ -d "${DIR}" ]; then
     (
-      cd "${DIR}" || exit 1
-      for TEST_FILENAME in ${TEST_FILENAMES}; do
-        if [ ! -r "${TEST_FILENAME}" ]; then
-          exit 1
-        fi
-      done
-      echo "#### Testing \"${DIR}\" ..."
-      bash -c "${RUN_COMMAND}"
-      echo
+      cd "${DIR}" >/dev/null 2>&1 || true
+      if ls ${TEST_FILENAMES} >/dev/null 2>&1; then
+        echo "#### Testing \"${DIR}\" ..."
+        bash -c "${RUN_COMMAND}"
+        echo
+      fi
     )
   fi
 done
