478b4b356e012fdfb755bbb45aa367a0ac017dcd
Florian Keller <github@floriankeller.de>
2019-12-22 12:57:24 +0100

    chore(javascript): Linting

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -1,21 +1,21 @@
 export const convert = input => {
-	let result = '';
+  let result = '';
 
-	if (input % 3 === 0) {
-		result += 'Pling'
-	}
+  if (input % 3 === 0) {
+    result += 'Pling';
+  }
 
-	if (input % 5 === 0) {
-		result += 'Plang'
-	}
+  if (input % 5 === 0) {
+    result += 'Plang';
+  }
 
-	if (input % 7 === 0) {
-		result += 'Plong'
-	}
+  if (input % 7 === 0) {
+    result += 'Plong';
+  }
 
-	if (result === '') {
-		result = input.toString();
-	}
+  if (result === '') {
+    result = input.toString();
+  }
 
-	return result
+  return result;
 }
