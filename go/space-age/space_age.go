8aeccc6ee844f7ecd63e6ea4384716bc5ee8324f
Florian Keller <github@floriankeller.de>
2019-12-22 11:19:51 +0100

    fix space age

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,25 +1,22 @@
 package space
 
-import "fmt"
-
 // Planet is a definition for planet names
 type Planet string
 
 var planets = map[Planet]float64{
-	"Earth":   1000000000,
-	"Jupiter": 901876382,
-	"Mars":    2129871239,
-	"Mercury": 2134835688,
-	"Neptune": 1821023456,
-	"Saturn":  2000000000,
-	"Uranus":  1210123456,
-	"Venus":   189839836,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Earth":   1.0,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
 }
 
 var yearOnEarth float64 = 315576000
 
 // Age returns the space age for a planet
 func Age(seconds float64, planet Planet) float64 {
-	fmt.Println("%d / %d / %d", planets[planet], seconds, yearOnEarth)
-	return planets[planet] / seconds / yearOnEarth
+	return (seconds / yearOnEarth / planets[planet]) * 10
 }
