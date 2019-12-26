3b81d9e236c8adf8e9edfb7e5bdd950e25e099f7
Florian Keller <github@floriankeller.de>
2019-12-26 12:22:57 +0100

    feat(javascript): Add solution for matching-brackets

---
+++ javascript/matching-brackets/matching-brackets.js
@@ -0,0 +1,21 @@
+const bracketPairs = [
+  ['[', ']'],
+  ['{', '}'],
+  ['(', ')'],
+];
+
+export const matchingBrackets = input => {
+  const stack = [];
+
+  for (const bracket of input.split('')) {
+    for (const [leftBracket, rightBracket] of bracketPairs) {
+      if (bracket === leftBracket) {
+        stack.push(leftBracket);
+      } else if (bracket === rightBracket && stack.pop() !== leftBracket) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
