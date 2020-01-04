b8ec0a04daad83637772e4616a0666bfb947aef0
Florian Keller <github@floriankeller.de>
2020-01-04 18:33:37 +0100

    feat(typescript): Add solution for hamming

---
+++ typescript/hamming/hamming.ts
@@ -0,0 +1,25 @@
+export default class Hamming {
+  compute(strand1: string, strand2: string): number {
+    if (!strand1 && strand2) {
+      throw new Error('left strand must not be empty');
+    }
+
+    if (strand1 && !strand2) {
+      throw new Error('right strand must not be empty');
+    }
+
+    if (strand1.length !== strand2.length) {
+      throw new Error('DNA strands must be of equal length.');
+    }
+
+    let diff = 0;
+
+    for (let i = 0; i < strand1.length; i++) {
+      if (strand1[i] !== strand2[i]) {
+        diff++;
+      }
+    }
+
+    return diff;
+  }
+}
