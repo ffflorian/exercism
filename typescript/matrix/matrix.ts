29e230b900694b8d701b3f3344768b4505ba6a93
Florian Keller <github@floriankeller.de>
2020-01-04 20:32:08 +0100

    feat(typescript): Add solution for matrix

---
+++ typescript/matrix/matrix.ts
@@ -0,0 +1,25 @@
+export default class Matrix {
+  get columns(): number[][] {
+    return this._columns;
+  }
+
+  get rows(): number[][] {
+    return this._rows;
+  }
+
+  private readonly _columns: number[][];
+  private readonly _rows: number[][];
+
+  constructor(input: string) {
+    this._rows = this._convertRows(input);
+    this._columns = this._convertColumns(this._rows);
+  }
+
+  _convertColumns(rows: number[][]): number[][] {
+    return rows[0].map((_, columnIndex) => rows.map(row => row[columnIndex]));
+  }
+
+  _convertRows(input: string): number[][] {
+    return input.split('\n').map(column => column.split(' ').map(number => parseInt(number)));
+  }
+}
