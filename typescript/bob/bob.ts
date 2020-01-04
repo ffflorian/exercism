45ffc11e871a76588822db2563cf54f9f3b869b5
Florian Keller <github@floriankeller.de>
2020-01-04 17:09:09 +0100

    feat(typescript): Add solution for bob

---
+++ typescript/bob/bob.ts
@@ -0,0 +1,22 @@
+export default class Bob {
+  hey(input: string): string {
+    input = input.trim();
+
+    if (input.length === 0) {
+      return 'Fine. Be that way!';
+    }
+
+    if (input === input.toUpperCase() && input.toUpperCase() !== input.toLowerCase()) {
+      if (input.endsWith('?')) {
+        return 'Calm down, I know what I\'m doing!';
+      }
+      return 'Whoa, chill out!';
+    }
+
+    if (input.slice(-1) === '?') {
+      return 'Sure.';
+    }
+
+    return 'Whatever.';
+  }
+}
