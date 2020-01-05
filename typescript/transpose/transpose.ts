81d1f569b4ffe588320e1d91174bf94e32a9468c
Florian Keller <github@floriankeller.de>
2020-01-05 13:09:43 +0100

    feat(typescript): Add solution for transpose

---
+++ typescript/transpose/transpose.ts
@@ -0,0 +1,15 @@
+export default class Transpose {
+  static transpose(lines: string[]): string[] {
+    return lines.reduce((result: string[], line, lineIndex) => {
+      line.split('').map((value, key) => {
+        if (typeof result[key] === 'undefined') {
+          result[key] = new Array(lineIndex + 1).join(' ');
+        }
+
+        result[key] += value;
+      });
+
+      return result;
+    }, []);
+  }
+}
