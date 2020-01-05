e0621f82502245737069ffacdbb275d01f3924c3
Florian Keller <github@floriankeller.de>
2020-01-05 12:43:49 +0100

    feat(typescript): Add solution for robot-name

---
+++ typescript/robot-name/robot-name.ts
@@ -0,0 +1,34 @@
+let usedNames: Record<string, boolean> = {};
+
+export default class Robot {
+  get name(): string {
+    return this._name;
+  }
+
+  static releaseNames(): void {
+    usedNames = {};
+  }
+
+  private _name!: string;
+
+  constructor() {
+    this.resetName();
+  }
+
+  resetName(): void {
+    this._name = this._generateName();
+  }
+
+  private _generateName(): string {
+    const randomString = () => String.fromCharCode(Math.floor(Math.random() * 25) + 65);
+    const num = Math.floor(Math.random() * 899) + 100;
+    const name = `${randomString()}${randomString()}${num}`;
+
+    if (typeof usedNames[name] === 'undefined') {
+      usedNames[name] = true;
+      return name;
+    }
+
+    return this._generateName();
+  }
+}
