d9998902c74161260729a6975fde6652411499b1
Florian Keller <github@floriankeller.de>
2020-01-05 11:44:56 +0100

    feat(typescript): Add solution for pascals-triangle

---
+++ typescript/pascals-triangle/pascals-triangle.ts
@@ -0,0 +1,29 @@
+export default class Triangle {
+  private readonly _rows: number[][];
+
+  constructor(length: number) {
+    this._rows = this._buildTriangle(length);
+  }
+
+  _buildTriangle(length: number): number[][] {
+    const triangle: number[][] = [];
+    for (let rowIndex = 0; rowIndex < length; rowIndex++) {
+      triangle[rowIndex] = [];
+      for (let leafIndex = 0; leafIndex <= rowIndex; leafIndex++) {
+        triangle[rowIndex][leafIndex] =
+          leafIndex === 0 || leafIndex === rowIndex
+            ? 1
+            : triangle[rowIndex - 1][leafIndex - 1] + triangle[rowIndex - 1][leafIndex];
+      }
+    }
+    return triangle;
+  }
+
+  get lastRow(): number[] | undefined {
+    return this.rows.pop();
+  }
+
+  get rows(): number[][] {
+    return this._rows;
+  }
+}
