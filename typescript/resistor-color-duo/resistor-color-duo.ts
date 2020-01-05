c9ba558823f0b44382a74b086b23ef3f5a6ba504
Florian Keller <github@floriankeller.de>
2020-01-05 12:33:38 +0100

    feat(typescript): Add solution for resistor-color-duo

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,20 @@
+export default class ResistorColor {
+  private readonly colorInputs: string[];
+  private readonly colors: string[];
+
+  constructor(colorInputs: string[]) {
+    this.colors = ['Black', 'Brown', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Violet', 'Grey', 'White'].map(color =>
+      color.toLowerCase()
+    );
+    this.colorInputs = colorInputs;
+    if (this.colorInputs.length < 2) {
+      throw new Error('At least two colors need to be present');
+    }
+  }
+
+  value(): number {
+    const [firstColor, secondColor] = this.colorInputs;
+    const value = `${this.colors.indexOf(firstColor)}${this.colors.indexOf(secondColor)}`;
+    return parseInt(value, 10);
+  }
+}
