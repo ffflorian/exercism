c68a7efb0d76e38bb2ac3d09ee29a1358398d44a
Florian Keller <github@floriankeller.de>
2020-01-05 12:55:00 +0100

    feat(typescript): Add solution for simple-cipher

---
+++ typescript/simple-cipher/simple-cipher.ts
@@ -0,0 +1,49 @@
+import * as crypto from 'crypto';
+
+export default class SimpleCipher {
+  get key(): string {
+    return this._key;
+  }
+
+  private readonly _key: string;
+  private readonly alphabet: string[];
+  private readonly keyIndizes: any;
+
+  constructor(key?: string) {
+    if (key && !/^[a-z]+$/.test(key)) {
+      throw new Error('Bad key');
+    }
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    this._key = key || this._generateRandomKey(100);
+    this.keyIndizes = this._key.split('').map(char => this.alphabet.indexOf(char));
+  }
+
+  _generateRandomKey(length: number): string {
+    const randomBytes = crypto.randomBytes(length);
+    let key = '';
+    for (let index = 0; index < length; index++) {
+      key += this.alphabet[Math.floor(randomBytes[index] / (256 / this.alphabet.length))];
+    }
+    return key;
+  }
+
+  decode(encodedText: string): string {
+    return encodedText
+      .split('')
+      .map((char, charIndex) => {
+        const keyPosition = this.alphabet.indexOf(char) - this.keyIndizes[charIndex % this.keyIndizes.length];
+        return this.alphabet[(keyPosition + 26) % 26];
+      })
+      .join('');
+  }
+
+  encode(clearText: string): string {
+    return clearText
+      .split('')
+      .map((char, charIndex) => {
+        const keyPosition = this.alphabet.indexOf(char) + this.keyIndizes[charIndex % this.keyIndizes.length];
+        return this.alphabet[keyPosition % 26];
+      })
+      .join('');
+  }
+}
