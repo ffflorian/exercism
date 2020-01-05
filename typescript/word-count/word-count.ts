5801b00556dc54362861095b685e586070a013a9
Florian Keller <github@floriankeller.de>
2020-01-05 13:15:11 +0100

    feat(typescript): Add solution for word-count

---
+++ typescript/word-count/word-count.ts
@@ -0,0 +1,12 @@
+export default class Words {
+  count(words: string): Map<string, number> {
+    return words
+      .trim()
+      .toLowerCase()
+      .split(/[\t\n ]+/)
+      .reduce((result, word) => {
+        result.set(word, result.has(word) ? result.get(word)! + 1 : 1);
+        return result;
+      }, new Map<string, number>());
+  }
+}
