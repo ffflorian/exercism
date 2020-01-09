bdeedf9fdc06790fe7844422b014b06d70e43aff
Florian Keller <github@floriankeller.de>
2020-01-09 20:13:47 +0100

    feat(javascript,typescript): Add solution for rotational-cipher

---
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -0,0 +1,9 @@
+export class RotationalCipher {
+  static rotate(input, shift) {
+    return input.replace(/[A-z]/g, letter => {
+      const charCode = letter.charCodeAt(0);
+      const startIndex = (charCode > 90) ? 97 : 65;
+      return String.fromCharCode(startIndex + ((letter.charCodeAt(0) - startIndex + shift) % 26));
+    })
+  }
+}
