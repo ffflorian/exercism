5817378a31207ad8225ccd2942e4247cdb31c693
Florian Keller <github@floriankeller.de>
2020-01-11 16:01:45 +0100

    chore(typescript): Linting

--- typescript/rotational-cipher/rotational-cipher.ts
+++ typescript/rotational-cipher/rotational-cipher.ts
@@ -2,8 +2,8 @@ export default class RotationalCipher {
   static rotate(input: string, shift: number): string {
     return input.replace(/[A-z]/g, letter => {
       const charCode = letter.charCodeAt(0);
-      const startIndex = (charCode > 90) ? 97 : 65;
+      const startIndex = charCode > 90 ? 97 : 65;
       return String.fromCharCode(startIndex + ((letter.charCodeAt(0) - startIndex + shift) % 26));
-    })
+    });
   }
 }
