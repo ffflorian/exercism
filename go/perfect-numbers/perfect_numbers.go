8a80e63e397831868ca3d76ca7273fe234879465
Florian Keller <github@floriankeller.de>
2020-01-12 17:59:28 +0100

    refactor(go): Improve soluton for perfect-numbers

--- go/perfect-numbers/perfect_numbers.go
+++ go/perfect-numbers/perfect_numbers.go
@@ -12,20 +12,18 @@ type Classification int
 var ErrOnlyPositive = errors.New("Classification is only possible for natural numbers")
 
 const (
-	// ClassificationError describes that there is an error
-	ClassificationError Classification = 0
 	// ClassificationPerfect describes that the number is perfect
-	ClassificationPerfect Classification = 1
+	ClassificationPerfect Classification = iota
 	// ClassificationAbundant describes that the number is abundant
-	ClassificationAbundant Classification = 2
+	ClassificationAbundant
 	// ClassificationDeficient describes that the number is deficient
-	ClassificationDeficient Classification = 3
+	ClassificationDeficient
 )
 
 // Classify determines if a number is perfect, abundant, or deficient
 func Classify(input int64) (Classification, error) {
 	if input < 1 {
-		return ClassificationError, ErrOnlyPositive
+		return -1, ErrOnlyPositive
 	}
 
 	var result int64 = 0
