b1057163daf8a476b83378adca48a0a7a497797b
Florian Keller <github@floriankeller.de>
2020-01-12 11:33:56 +0100

    feat(go): Improve solution for isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,20 +1,17 @@
 package isogram
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 // IsIsogram determines if a word or phrase is an isogram.
 func IsIsogram(input string) bool {
-	known := make([]rune, len(input))
-	array := []rune(strings.ToLower(input))
+	input = strings.ToUpper(input)
 
-	for _, newRune := range array {
-		for _, knownRune := range known {
-			if knownRune == newRune {
-				return false
-			}
-		}
-		if newRune >= 97 && newRune <= 122 {
-			known = append(known, newRune)
+	for _, char := range input {
+		if unicode.IsLetter(char) && strings.Count(input, string(char)) > 1 {
+			return false
 		}
 	}
 
