dbd031ca1ea512e352929444330cec1ee46603ab
Florian Keller <github@floriankeller.de>
2020-01-13 16:29:09 +0100

    chore: Simplify readme update script

--- bin/update_readme.sh
+++ bin/update_readme.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 
-GO_SOLUTIONS=$(cd go && ls -d */ | wc -l)
-JS_SOLUTIONS=$(cd javascript && ls -d */ | wc -l)
-TS_SOLUTIONS=$(cd typescript && ls -d */ | wc -l)
-PS_SOLUTIONS=$(cd purescript && ls -d */ | wc -l)
-PYTHON_SOLUTIONS=$(cd python && ls -d */ | wc -l)
-RUST_SOLUTIONS=$(cd rust && ls -d */ | wc -l)
-BASH_SOLUTIONS=$(cd bash && ls -d */ | wc -l)
+update_line() {
+  DIR="${1}"
+  SOLUTIONS_COUNT=$(cd "${DIR}" && ls -d */ | wc -l)
+  sed -i "s/\(.\/${DIR})\).* \[/\1 (${SOLUTIONS_COUNT} solutions) \[/g" README.md
+}
 
-sed -i "s/\(.\/go)\).* \[/\1 (${GO_SOLUTIONS} solutions) \[/g" README.md
-sed -i "s/\(.\/javascript)\).* \[/\1 (${JS_SOLUTIONS} solutions) \[/g" README.md
-sed -i "s/\(.\/typescript)\).* \[/\1 (${TS_SOLUTIONS} solutions) \[/g" README.md
-sed -i "s/\(.\/purescript)\).* \[/\1 (${PS_SOLUTIONS} solution) \[/g" README.md
-sed -i "s/\(.\/python)\).* \[/\1 (${PYTHON_SOLUTIONS} solutions) \[/g" README.md
-sed -i "s/\(.\/rust)\).* \[/\1 (${RUST_SOLUTIONS} solutions) \[/g" README.md
-sed -i "s/\(.\/bash)\).* \[/\1 (${BASH_SOLUTIONS} solutions) \[/g" README.md
+update_line go
+update_line javascript
+update_line purescript
+update_line python
+update_line rust
+update_line typescript
 
-if [ ! -z "$(git status --porcelain)" ]; then
-  git config --global "user.email" "${GIT_AUTHOR_EMAIL}"
-  git config --global "user.name" "${GIT_AUTHOR_NAME}"
+if [ -n "$(git status --porcelain)" ]; then
   REPO_URL="$(git remote get-url origin | sed -n "s/https:\/\/github.com\//https:\/\/${GIT_AUTHOR_NAME}:${GH_TOKEN}@github.com\//p")"
+  git config "user.email" "${GIT_AUTHOR_EMAIL}"
+  git config "user.name" "${GIT_AUTHOR_NAME}"
   git remote set-url origin "${REPO_URL}"
   git checkout master
-  git add README.md
-  git commit -m "chore(docs): Update readme [ci skip]"
+  git commit -m "chore(docs): Update readme [ci skip]" README.md
   git push
 fi
