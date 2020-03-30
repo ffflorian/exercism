9d05bd3bf93f08528f47c73b2b81099e101056b3
Florian Keller <github@floriankeller.de>
2020-03-30 14:11:22 +0200

    linting

--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,9 +1,6 @@
 export default class ArmstrongNumbers {
   static isArmstrongNumber(candidate: number): boolean {
-    const candidateArray = candidate
-      .toString()
-      .split('')
-      .map(Number);
+    const candidateArray = candidate.toString().split('').map(Number);
     const result = candidateArray.reduce((acc, n) => acc + Math.pow(n, candidateArray.length), 0);
     return result === candidate;
   }
