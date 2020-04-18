3540ea57f7b45d4c3f51d62e4fee633eae79ddc5
Florian Keller <github@floriankeller.de>
2020-04-18 16:59:20 +0200

    fix(typescript): Linting

--- typescript/accumulate/accumulate.ts
+++ typescript/accumulate/accumulate.ts
@@ -1,8 +1,8 @@
-export default function accumulate<V, A extends Array<V>>(array: A, accumulator: (value: V) => any) {
+export default function accumulate<V, A extends V[]>(array: A, accumulator: (value: V) => any): any[] {
   const result = [];
   for (let index = 0; index < array.length; index++) {
     const processedValue = accumulator(array[index]);
     result.push(processedValue);
   }
   return result;
-};
+}
