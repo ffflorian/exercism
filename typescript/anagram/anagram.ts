480f11ae8626a5112a84621befe4798c4b8d83e0
Florian Keller <github@floriankeller.de>
2020-04-19 08:30:11 +0200

    feat(typescript): Add solution for anagram

---
+++ typescript/anagram/anagram.ts
@@ -0,0 +1,20 @@
+export default class Anagram {
+  public word: string;
+
+  constructor(word: string) {
+    this.word = word;
+  }
+
+  public matches(...matches: string[]): string[] {
+    return matches.reduce((result: string[], matchTest) => {
+      if (this.word.toLowerCase() !== matchTest.toLowerCase() && this.sort(this.word) === this.sort(matchTest)) {
+        result.push(matchTest);
+      }
+      return result;
+    }, []);
+  }
+
+  private sort(word: string): string {
+    return word.toLowerCase().split('').sort().join('');
+  }
+}
