12f31f93c4ff88a1ee2d8d0a23b653f1429a15f1
Florian Imdahl <git@ffflorian.de>
2021-08-05 20:48:39 +0200

    feat(javascript): Add solution for bank-account

---
+++ javascript/bank-account/bank-account.js
@@ -0,0 +1,55 @@
+export class BankAccount {
+  #isOpen;
+  #balance;
+
+  constructor() {
+    this.#balance = 0;
+  }
+
+  open() {
+    if (this.#isOpen) {
+      throw new ValueError();
+    }
+    this.#isOpen = true;
+  }
+
+  close() {
+    if (!this.#isOpen) {
+      throw new ValueError();
+    }
+    this.#isOpen = false;
+  }
+
+  deposit(value) {
+    if (!this.isOpen || value < 0) {
+      throw new ValueError();
+    }
+
+    this.#balance += value;
+  }
+
+  withdraw(value) {
+    if (!this.isOpen || value > this.balance || value < 0) {
+      throw new ValueError();
+    }
+
+    this.#balance -= value;
+  }
+
+  get isOpen() {
+    return this.#isOpen;
+  }
+
+  get balance() {
+    if (!this.#isOpen) {
+      throw new ValueError();
+    }
+    return this.#balance;
+  }
+}
+
+export class ValueError extends Error {
+  constructor() {
+    super('Bank account error');
+  }
+}
