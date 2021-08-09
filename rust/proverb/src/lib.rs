4fc1fbe66d457a5ca63dbca08071b1bc870072d7
Florian Imdahl <git@ffflorian.de>
2021-08-09 16:22:20 +0200

    fix(rust): Use String::new() in proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn build_proverb(list: &[&str]) -> String {
     list.into_iter()
         .enumerate()
-        .fold(String::from(""), |acc, (i, cur)| {
+        .fold(String::new(), |acc, (i, cur)| {
             match list.get(i + 1) {
                 Some(s) => format!("{}For want of a {} the {} was lost.\n", acc, cur, s),
                 None => format!("{}And all for the want of a {}.", acc, list[0]),
