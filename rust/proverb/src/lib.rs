a686a3498da1de43571a12ef795fbf2c2d4bd1c4
Florian Imdahl <git@ffflorian.de>
2021-08-09 16:20:58 +0200

    feat(rust): Add solution for proverb

---
+++ rust/proverb/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    list.into_iter()
+        .enumerate()
+        .fold(String::from(""), |acc, (i, cur)| {
+            match list.get(i + 1) {
+                Some(s) => format!("{}For want of a {} the {} was lost.\n", acc, cur, s),
+                None => format!("{}And all for the want of a {}.", acc, list[0]),
+            }
+        })
+}
