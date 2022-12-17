3ded1ea3cfdfcffc0ab6a86af86ed231fc8ff91c
Florian Imdahl <git@ffflorian.de>
2022-12-17 20:47:38 +0100

    fix(javascript) nucleotide-count update

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,14 +1,12 @@
-export class NucleotideCounts {
-  static parse(strand) {
-    if (!/^[ACGT]*$/.test(strand)) {
-      throw new Error('Invalid nucleotide in strand');
-    }
-
-    const adenineCount = (strand.match(/[A]/g) || []).length;
-    const cytosineCount = (strand.match(/[C]/g) || []).length;
-    const guanineCount = (strand.match(/[G]/g) || []).length;
-    const thymineCount = (strand.match(/[T]/g) || []).length;
-
-    return `${adenineCount} ${cytosineCount} ${guanineCount} ${thymineCount}`;
+export function countNucleotides(strand) {
+  if (!/^[ACGT]*$/.test(strand)) {
+    throw new Error('Invalid nucleotide in strand');
   }
+
+  const adenineCount = (strand.match(/[A]/g) || []).length;
+  const cytosineCount = (strand.match(/[C]/g) || []).length;
+  const guanineCount = (strand.match(/[G]/g) || []).length;
+  const thymineCount = (strand.match(/[T]/g) || []).length;
+
+  return `${adenineCount} ${cytosineCount} ${guanineCount} ${thymineCount}`;
 }
