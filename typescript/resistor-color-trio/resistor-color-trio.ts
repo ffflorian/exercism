2b0bbd4dfca696cbe13a979fe5e6fdaa945a5a83
Florian Imdahl <git@ffflorian.de>
2023-02-21 20:40:37 +0100

    feat(typescript): Add resistor-color-trio

---
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -0,0 +1,46 @@
+// prettier-ignore
+const colors = [
+  'Black',
+  'Brown',
+  'Red',
+  'Orange',
+  'Yellow',
+  'Green',
+  'Blue',
+  'Violet',
+  'Grey',
+  'White',
+].map(color => color.toLowerCase())
+
+const prefixes = [
+  {
+    unit: 'giga',
+    value: 1_000_000_000,
+  },
+  {
+    unit: 'mega',
+    value: 1_000_000,
+  },
+  {
+    unit: 'kilo',
+    value: 1_000,
+  },
+];
+
+export function decodedResistorValue(colorInputs: string[]) {
+  const [firstColor, secondColor, thirdColor] = colorInputs;
+
+  const zerosCount = colors.indexOf(thirdColor);
+  let value = parseInt(`${colors.indexOf(firstColor)}${colors.indexOf(secondColor)}${'0'.repeat(zerosCount)}`, 10);
+  let unit = 'ohms';
+
+  for (let prefix of prefixes) {
+    if (value >= prefix.value) {
+      value /= prefix.value;
+      unit = `${prefix.unit}${unit}`;
+      break;
+    }
+  }
+
+  return `${value} ${unit}`;
+}
